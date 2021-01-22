46681e749503139154bbeb57aeab5110f8ab7bda
Pyae Sone <mgmgpyaesonewin@gmail.com>
2021-01-22 23:23:26 +0630

    deploy script updated

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,12 @@
+echo "Login Successful"
 cd /var/www/html/
+sudo git reset --hard HEAD
 sudo git pull origin master
+sudo npm install
 sudo npm run build
+echo "FrontEnd Deploy Successfully ...."
 cd ~/api.gcrawler.site/
 git pull origin main
 npm run build
 pm2 restart main
+echo "API Deploy Successfully ...."
