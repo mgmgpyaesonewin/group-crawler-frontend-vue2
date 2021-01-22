c7b4ffa3f7507df5307e1723bddc9c6691aedeb0
Pyae Sone <mgmgpyaesonewin@gmail.com>
2021-01-22 10:50:04 +0630

    tailwind purge removed

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     // removeDeprecatedGapUtilities: true,
     // purgeLayersByDefault: true,
   },
-  purge: ["./src/**/*.vue"],
+  purge: false,
   theme: {
     extend: {},
   },
